212001f63e8b402a129594162c2ba9f17cce863d
Vagner <vagnerlucion@gmail.com>
2019-08-10 19:54:18 -0300

    finished to send

--- root/js/index.js
+++ root/js/index.js
@@ -1,5 +1,6 @@
 function handleButtonClick() {
     let button = document.getElementById("finish-button");
-    button.classList.toggle('afterClickButton');
+    button.classList.remove('beforeClickButton');
+    button.classList.add('afterClickButton');
     button.innerHTML = "Obrigado!";
 }
